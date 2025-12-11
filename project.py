f1d8bcebbc68a07d2152c01fadbfb6c95d30aafc
taniaarevalo <119436420+taniaarevalo@users.noreply.github.com>
2025-12-11 14:18:44 +0100

    new version with vpool

--- project.py
+++ project.py
@@ -1,79 +1,104 @@
-from pysat.formula import CNFPlus
+from pysat.formula import CNFPlus, IDPool
 from pysat.solvers import Minicard
 
 
-def func_DEP_t(T, N, DEP_t, dep, formula):
-    """To add clause DEPt in cnf."""
+def id_dep(T, N, vpool):
+    """Will return a dictionnary with all the id for dep. {(t, p, s): id}"""
+    dep = {}
     for t in range(T):
         for p in range(N):
-            for s in range(2):
-                formula.append([-DEP_t[t], dep(t, p, s)])
-                formula.append([dep(t, p, s), DEP_t[t]])
+            for s in range(2):  # two ways only : 0 = "Aller", 1 = "Retour"
+                dep[(t, p, s)] = vpool.id(("dep", t, p, s))
+    return dep
 
 
-def func_ARR_t(T, durations, ARR_t, dur, formula):
-    """To add clause ARRt in cnf."""
+def id_A(T, N, vpool):
+    """Will return a dictionnary with all the id for A. {(t, p): id}"""
+    A = {}
+    for t in range(T):
+        for p in range(N):
+            A[(p, t)] = vpool.id(("A", p, t))
+    return A
+
+
+def id_B(T, N, vpool):
+    """Will return a dictionnary with all the id for B. {(t, p): id}"""
+    B = {}
+    for t in range(T):
+        for p in range(N):
+            B[(p, t)] = vpool.id(("B", p, t))
+    return B
+
+
+def id_dur(T, durations, vpool):
+    """Will return a dictionnary with all the id for dur. {(t, d): id}"""
+    dur = {}
     for t in range(T):
         for d in durations:
-            formula.append([-ARR_t[t], dur[]])
+            dur[(t, d)] = vpool.id(("dur", t, d))
+    return dur
+
+
+def id_side(T, vpool):
+    """Will return a dictionnary with all the id for side. {t: id}"""
+    side = {}
+    for t in range(T):
+        side[t] = vpool.id(("side", t))
+    return side
+
+
+def id_DEP(T, vpool):
+    """Will return a dictionnary with all the id for DEP. {t: id}"""
+    DEP = {}
+    for t in range(T):
+        DEP[t] = vpool.id(("DEP", t))
+    return DEP
+
+
+def id_ARR(T, vpool):
+    """Will return a dictionnary with all the id for ARR. {t: id}"""
+    ARR = {}
+    for t in range(T):
+        ARR[t] = vpool.id(("ARR", t))
+    return ARR
+
+def id_ALL(T, vpool):
+    """Will return a dictionnary with all the id for ALL. {t: id}"""
+    ALL = {}
+    for t in range(T):
+        ALL[t] = vpool.id(("ALL", t))
+    return ALL
+
 
 
 def gen_solution(durations: list[int], c: int, T: int) -> None | list[tuple]:
     N = len(durations)          # number of chicken 
-    idCounter = 1               # increment counter to get new ID
+    formula = CNFPlus()         # we can start the CNF
+    vpool = IDPool()            # to associate variable to a SAT number
 
-    # diccos to contain the ID for each variable
-    dep = {}
-    a = {}
-    b = {}
-    dur = {}
-    side = {}
-    DEP_t = {}
-    ARR_t = {}
-    ALL_t = {}
+    #dep(t, p, s)
+    dep = id_dep(T, N, vpool)
 
-    # put a id number on each variable
-    # we check for each time > each chicken > each direction:
-    for t in range(T):
-        side[t] = idCounter
-        idCounter += 1
+    #A(p, t)
+    A = id_A(T, N, vpool)
 
-        DEP_t[t] = idCounter 
-        idCounter += 1
+    #B(p, t)
+    B = id_B(T, N, vpool)
 
-        ARR_t[t] = idCounter 
-        idCounter += 1
+    #dur(t, d)
+    dur = id_dur(T, durations, vpool)
 
-        ALL_t[t] = idCounter 
-        idCounter += 1
+    #side(t)
+    side = id_side(T, vpool)
 
-        for p in range(N):
+    #DEP(t)
+    DEP = id_DEP(T, vpool)
 
-            for s in range(2): # 0: Aller, 1: Retour
-                dep[(t, p, s)] = idCounter
-                idCounter += 1
+    #ARR(t)
+    ARR = id_ARR(T, vpool)
 
-            a[(p, t)] = idCounter
-            idCounter += 1
-
-            b[(p, t)] = idCounter
-            idCounter += 1
-
-            for d in range(durations[p]): # durées possibles
-                dur[(t, d)] = idCounter
-                idCounter += 1
-        
-        # we can start the CNF
-        formula = CNFPlus()
-
-        #DEPt
-        func_DEP_t(T, N, DEP_t, dep, formula)
-
-        #ARRt
-        func_ARR_t(T, durations, ARR_t, dur, formula)
-
-
-
+    #ALL(t)
+    ALL = id_ALL(T, vpool)
 
 def find_duration(durations: list[int], c: int) -> int:
     pass
